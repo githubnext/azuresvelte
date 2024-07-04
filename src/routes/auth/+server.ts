407021ced8b2355c24efe6d77a10d3b0dc7ffcc1
Idan Gazit <idan@gazit.me>
2024-07-04 16:20:03 +0300

    iterate over entries

--- src/routes/auth/+server.ts
+++ src/routes/auth/+server.ts
@@ -1,6 +1,7 @@
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const POST: RequestHandler = async ({request, platform}) => {
-	return json({request, platform});
+export const POST: RequestHandler = async (e) => {
+	console.log([...e.request.headers.entries()])
+	return json([...e.request.headers.entries()]);
 }
