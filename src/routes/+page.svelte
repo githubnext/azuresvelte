43605214bbe7a05b7902d79586d4f0973ba8ab92
Idan Gazit <idan@gazit.me>
2024-07-04 16:33:42 +0300

    bump v3

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<h1>v2</h1>
+<h1>v3</h1>
 
 <form method="POST" action="/auth?/login">
 	<button>Login</button>
