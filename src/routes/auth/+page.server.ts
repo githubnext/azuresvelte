9512e4bccbcd7f72456be57e849469b7a446fe05
Idan Gazit <idan@gazit.me>
2024-07-04 11:02:05 +0300

    Add appinsights

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,8 +1,20 @@
 import type { Actions } from './$types';
 
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+import { env } from '$env/dynamic/private';
+
+let appInsights: ApplicationInsights;
+if ('APPLICATIONINSIGHTS_CONNECTION_STRING' in env) {
+	appInsights = new ApplicationInsights({ config: {
+		connectionString: env.APPLICATIONINSIGHTS_CONNECTION_STRING
+	} });
+}
+
 export const actions = {
 	login: async (event) => {
 		console.error("THE EVENT: ", event, event.request);
+		appInsights?.trackTrace({message: 'login event', properties: {event}});
 		return {action: 'login'};
 	},
 	logout: async (event) => {
